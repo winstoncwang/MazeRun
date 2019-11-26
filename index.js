781a42c7ce36224b9f166b35f59d3a20da669b71
winstoncwang <congwang@live.co.uk>
2019-11-26 16:12:37 +0000

    Version 0.12
    reuse demo as boilerplate code for maze layout

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('hi there');
+const { Engine, Render, Runner, World, Bodies } = Matter; //deconstruct the Matter object
+
+//create engine
+
+const engine = Engine.create();
+const { world } = engine;
+
+const wallThickness = 20;
+const width = 600;
+const height = 600;
+
+//create render property
+
+const render = Render.create({
+	element : document.body,
+	engine  : engine,
+	options : {
+		wireframes : false,
+		width,
+		height
+	}
+});
+
+//run the rendering
+Render.run(render);
+Runner.run(Runner.create(), engine); //alias for Matter.Runner.start
+
+//Matter.Bodies.rectangle(x, y, width, height)
+//Bodies object creates the shape while Body object applys the physical properties
+
+//adding Walls
+const walls = [
+	Bodies.rectangle(width / 2, 0, width, wallThickness, { isStatic: true }), //top
+	Bodies.rectangle(width / 2, height, width, wallThickness, { isStatic: true }), //bottom
+	Bodies.rectangle(0, height / 2, wallThickness, height, { isStatic: true }), //left
+	Bodies.rectangle(width, height / 2, wallThickness, height, { isStatic: true }) //right
+];
+
+//adding shape and world together
+World.add(world, walls);
