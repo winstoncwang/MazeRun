3cef3a4b866623155747d9d933d54b5f59e8ab40
winstoncwang <congwang@live.co.uk>
2019-11-28 18:25:09 +0000

    Version 0.13
    grid array which tracks the cells which we've visited

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { Engine, Render, Runner, World, Bodies } = Matter; //deconstruct the Matter object
+const {
+    Engine,
+    Render,
+    Runner,
+    World,
+    Bodies
+} = Matter; //deconstruct the Matter object
 
 //create engine
 
 const engine = Engine.create();
-const { world } = engine;
+const {
+    world
+} = engine;
 
 const wallThickness = 20;
 const width = 600;
@@ -12,13 +20,13 @@ const height = 600;
 //create render property
 
 const render = Render.create({
-	element : document.body,
-	engine  : engine,
-	options : {
-		wireframes : false,
-		width,
-		height
-	}
+    element: document.body,
+    engine: engine,
+    options: {
+        wireframes: true,
+        width,
+        height
+    }
 });
 
 //run the rendering
@@ -30,11 +38,23 @@ Runner.run(Runner.create(), engine); //alias for Matter.Runner.start
 
 //adding Walls
 const walls = [
-	Bodies.rectangle(width / 2, 0, width, wallThickness, { isStatic: true }), //top
-	Bodies.rectangle(width / 2, height, width, wallThickness, { isStatic: true }), //bottom
-	Bodies.rectangle(0, height / 2, wallThickness, height, { isStatic: true }), //left
-	Bodies.rectangle(width, height / 2, wallThickness, height, { isStatic: true }) //right
+    Bodies.rectangle(width / 2, 0, width, wallThickness, {
+        isStatic: true
+    }), //top
+    Bodies.rectangle(width / 2, height, width, wallThickness, {
+        isStatic: true
+    }), //bottom
+    Bodies.rectangle(0, height / 2, wallThickness, height, {
+        isStatic: true
+    }), //left
+    Bodies.rectangle(width, height / 2, wallThickness, height, {
+        isStatic: true
+    }) //right
 ];
 
 //adding shape and world together
 World.add(world, walls);
+
+//maze generation
+
+const grid = Array(3).fill(null).map(() => Array(3).fill(false));
