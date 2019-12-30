1f2cc4864c556862dc177e4cbfb7a08ee7bf9736
winstoncwang <congwang@live.co.uk>
2019-12-30 15:35:26 +0000

    Version 0.24
    movement update and velocity changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter; //deconstruct the Matter object
+const { Engine, Render, Runner, World, Bodies, Body } = Matter; //deconstruct the Matter object
 
 //create engine
 
@@ -206,22 +206,23 @@ const ball = Bodies.circle(
 World.add(world, ball);
 
 //tracking key press and move ball
+
 document.addEventListener('keydown', (event) => {
-	console.log(event.keyCode);
+	const { x, y } = ball.velocity;
 	//move up
 	if (event.keyCode === 38 || event.keyCode === 87) {
-		console.log('up');
+		Body.setVelocity(ball, { x, y: y - 6 });
 	}
 	//move down
 	if (event.keyCode === 40 || event.keyCode === 83) {
-		console.log('down');
+		Body.setVelocity(ball, { x, y: y + 6 });
 	}
 	//move left
 	if (event.keyCode === 37 || event.keyCode === 65) {
-		console.log('left');
+		Body.setVelocity(ball, { x: x - 6, y });
 	}
 	//move right
 	if (event.keyCode === 39 || event.keyCode === 68) {
-		console.log('right');
+		Body.setVelocity(ball, { x: x + 6, y });
 	}
 });
