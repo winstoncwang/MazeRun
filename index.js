fec1282e62a4b4b4b0fbeeb4af3b90070ea4e0f3
winstoncwang <congwang@live.co.uk>
2020-01-16 11:23:42 +0000

    Bug fix
    add function to restrict ball velocity

--- index.js
+++ index.js
@@ -232,25 +232,42 @@ const ball = Bodies.circle(
 
 World.add(world, ball);
 
+//Function that checks velocity
+const checkVelocity = (velX, velY) => {
+	console.log(velX, velY); //check speed does not exceed +/-20
+	if (velX > 20 || velX < -20 || velY > 20 || velY < -20) {
+		return false;
+	}
+	return true;
+};
+
 //tracking key press and move ball
 
 document.addEventListener('keydown', (event) => {
 	const { x, y } = ball.velocity;
 	//move up
 	if (event.keyCode === 38 || event.keyCode === 87) {
-		Body.setVelocity(ball, { x, y: y - 4 });
+		if (checkVelocity(x, y)) {
+			Body.setVelocity(ball, { x, y: y - 4 });
+		}
 	}
 	//move down
 	if (event.keyCode === 40 || event.keyCode === 83) {
-		Body.setVelocity(ball, { x, y: y + 4 });
+		if (checkVelocity(x, y)) {
+			Body.setVelocity(ball, { x, y: y + 4 });
+		}
 	}
 	//move left
 	if (event.keyCode === 37 || event.keyCode === 65) {
-		Body.setVelocity(ball, { x: x - 4, y });
+		if (checkVelocity(x, y)) {
+			Body.setVelocity(ball, { x: x - 4, y });
+		}
 	}
 	//move right
 	if (event.keyCode === 39 || event.keyCode === 68) {
-		Body.setVelocity(ball, { x: x + 4, y });
+		if (checkVelocity(x, y)) {
+			Body.setVelocity(ball, { x: x + 4, y });
+		}
 	}
 });
 
