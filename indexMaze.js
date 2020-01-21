f779f67afc9cc1750e27263212e6e0e09fc8cb8b
winstoncwang <congwang@live.co.uk>
2020-01-21 15:25:53 +0000

    File organisation
    delete duplicates

--- indexMaze.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.querySelector('canvas');
-const body = document.querySelector('body');
-const win = document.querySelector('.win');
-const resetButton = document.querySelector('.restart');
-
-const mazeOne = new mazeRunner(canvas, body, win, resetButton);
-mazeOne.generateMaze();
